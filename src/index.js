1f73305c0905addb700dcf1bc5ceaa2e69fc4988
Ankit Srivastava <gamear007@gmail.com>
2021-11-18 11:09:45 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,29 +5,14 @@ import App from "./App";
 import { Provider } from "react-redux";
 import store from "./state/store";
 
-// import ReactGA from 'react-ga';
-// ReactGA.initialize('G-8W242QMKEK');
-// ReactGA.pageview(window.location.pathname + window.location.search);
-const injectGA = () => {
-  if (typeof window == 'undefined') {
-    return;
-  }
-  window.dataLayer = window.dataLayer || [];
-  function gtag() {
-    window.dataLayer.push(arguments);
-  }
-  gtag('js', new Date());
+import ReactGA from 'react-ga';
+ReactGA.initialize('G-8W242QMKEK');
+ReactGA.pageview(window.location.pathname + window.location.search);
 
-  gtag('config', 'G-8W242QMKEK');
-};
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-   <script
-      async
-      src="https://www.googletagmanager.com/gtag/js?id=G-8W242QMKEK"
-    />
-    <script>{injectGA()}</script>
+   
       <App />
     </Provider>
   </React.StrictMode>,
