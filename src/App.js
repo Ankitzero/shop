340721265728b1a6668d3e25fe7d495ffaa1d5bb
Ankit Srivastava <gamear007@gmail.com>
2021-11-18 12:00:58 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,11 +2,6 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import AddShops from "./components/AddShops";
 import NavBar from "./components/NavBar";
 import Home from "./components/Home";
-import RouteChangeTracker from './RouteChangeTracker' 
-import ReactGA from 'react-ga';
-// ReactGA.pageview(window.location.pathname + window.location.search);
-const TRACKING_ID = "G-NZ3PRLBMZQ"; // YOUR_OWN_TRACKING_ID
-ReactGA.initialize(TRACKING_ID);
 // import InlineStyleComponent from "./examples/InlineStyleComponent";
 function App() {
   <RouteChangeTracker />
